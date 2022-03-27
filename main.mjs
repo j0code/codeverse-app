4e01c885d1a2ab7b7f67af48784738f73d1234fc
j0code <jonaslp759@gmail.com>
2022-03-27 21:57:22 +0200

    npm init + config-loader + fix oopsies

--- main.mjs
+++ main.mjs
@@ -3,14 +3,16 @@ import https from "https"
 import express from "express"
 import * as iolib from "socket.io"
 import sql from "./sql.mjs"
+import config from "./config-loader.mjs"
 
 export default class CodeverseApp extends EventEmitter {
 
 	#app; #httpsServer; #io; #sql
-	constructor(config) {
+	constructor() {
+		super()
 		this.#app = express()
-		this.#httpsServer = https.createServer(config.https, app)
-		this.#io = new iolib.Server(httpsServer)
+		this.#httpsServer = https.createServer(config.https, this.#app)
+		this.#io = new iolib.Server(this.#httpsServer)
 		this.#sql = sql(config.sql)
 
 		this.#httpsServer.listen(config.port, () => { console.log(`Server is running on ${config.port}`) }) // debug log
